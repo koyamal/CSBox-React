f6c73494bb9ce550c939dec5d66e0fcf64bd3fb5
koyamal <74910401+koyamal@users.noreply.github.com>
2023-04-13 08:32:21 +0900

    Use onClick in button

--- src/App.jsx
+++ src/App.jsx
@@ -1,62 +1,16 @@
 import React, { useState } from "react";
 import "./styles.css";
-import { InputTodo } from "./components/InputTodo";
-import { IncompleteTodos } from "./components/IncompleteTodos";
-import { CompleteTodos } from "./components/CompleteTodos";
 
 export const App = () => {
-  const [todoText, setTodoText] = useState("");
-  const [incompleteTodos, setIncompleteTodos] = useState(["ToDo 1", "ToDo 2"]);
-  const [completeTodos, setCompleteTodos] = useState(["ToDo 3", "ToDo 4"]);
-
-  const onChangeTodoText = (event) => {
-    setTodoText(event.target.value);
+  const [count, setCount] = useState(0);
+  const onClickUp = () => {
+    setCount(count + 1);
   };
-
-  const onClickAdd = () => {
-    if (todoText === "") {
-      return;
-    }
-    const newTodos = [...incompleteTodos, todoText];
-    setIncompleteTodos(newTodos);
-    setTodoText("");
-  };
-
-  const onClickDelete = (index) => {
-    const newTodos = [...incompleteTodos];
-    newTodos.splice(index, 1);
-    setIncompleteTodos(newTodos);
-  };
-
-  const onClickComplete = (index) => {
-    const newIncompleteTodos = [...incompleteTodos];
-    newIncompleteTodos.splice(index, 1);
-    setIncompleteTodos(newIncompleteTodos);
-    const newCompleteTodos = [...completeTodos, incompleteTodos[index]];
-    setCompleteTodos(newCompleteTodos);
-  };
-
-  const onClickBack = (index) => {
-    const newCompleteTodos = [...completeTodos];
-    newCompleteTodos.splice(index, 1);
-    setCompleteTodos(newCompleteTodos);
-    const newIncompleteTodos = [...incompleteTodos, completeTodos[index]];
-    setIncompleteTodos(newIncompleteTodos);
-  };
-
   return (
-    <>
-      <InputTodo
-        todoText={todoText}
-        onChangeTodoText={onChangeTodoText}
-        onClickAdd={onClickAdd}
-      />
-      <IncompleteTodos
-        incompleteTodos={incompleteTodos}
-        onClickComplete={onClickComplete}
-        onClickDelete={onClickDelete}
-      />
-      <CompleteTodos completeTodos={completeTodos} onClickBack={onClickBack} />
-    </>
+    <div>
+      <h1>Hello</h1>
+      <p>{count}</p>
+      <button onClick={onClickUp}>Up</button>
+    </div>
   );
 };
