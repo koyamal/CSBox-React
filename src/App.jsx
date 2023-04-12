dcf66e735134a18bd0273c7e323ae2f451508749
koyamal <74910401+koyamal@users.noreply.github.com>
2023-04-12 16:07:00 +0900

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <React.Fragment>
+      <h1>Hello</h1>
+      <p>Good</p>
+    </React.Fragment>
+  );
+};
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDom from "react-dom";
+import App from "./App";
+
+// const App = () => {
+//   return (
+//     <React.Fragment>
+//       <h1>Hello</h1>
+//       <p>Good</p>
+//     </React.Fragment>
+//   );
+// };
+
+ReactDom.render(<App />, document.getElementById("root"));
