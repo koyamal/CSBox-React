8d7718f12e947e1d2bccf19b2a6d03f21bbb1178
koyamal <74910401+koyamal@users.noreply.github.com>
2023-04-12 17:56:25 +0900

    Create Todo App with React

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,62 @@
-/* eslint react-hooks/exhaustive-deps: off */
-import React, { useEffect, useState } from "react";
-import { ColorfulMessage } from "./components/ColorfulMessage";
+import React, { useState } from "react";
+import "./styles.css";
+import { InputTodo } from "./components/InputTodo";
+import { IncompleteTodos } from "./components/IncompleteTodos";
+import { CompleteTodos } from "./components/CompleteTodos";
 
-const App = () => {
-  console.log("Top of App");
-  const [num, setNum] = useState(0);
-  const [faceShowFlag, setFaceShowFlag] = useState(true);
-  const onClickCountUp = () => {
-    setNum(num + 1);
+export const App = () => {
+  const [todoText, setTodoText] = useState("");
+  const [incompleteTodos, setIncompleteTodos] = useState(["ToDo 1", "ToDo 2"]);
+  const [completeTodos, setCompleteTodos] = useState(["ToDo 3", "ToDo 4"]);
+
+  const onChangeTodoText = (event) => {
+    setTodoText(event.target.value);
   };
-  const onClickShowFlag = () => {
-    setFaceShowFlag(!faceShowFlag);
-  };
-  useEffect(() => {
-    if (num % 3 === 0) {
-      faceShowFlag || setFaceShowFlag(true);
-    } else {
-      faceShowFlag && setFaceShowFlag(false);
+
+  const onClickAdd = () => {
+    if (todoText === "") {
+      return;
     }
-  }, [num]);
+    const newTodos = [...incompleteTodos, todoText];
+    setIncompleteTodos(newTodos);
+    setTodoText("");
+  };
+
+  const onClickDelete = (index) => {
+    const newTodos = [...incompleteTodos];
+    newTodos.splice(index, 1);
+    setIncompleteTodos(newTodos);
+  };
+
+  const onClickComplete = (index) => {
+    const newIncompleteTodos = [...incompleteTodos];
+    newIncompleteTodos.splice(index, 1);
+    setIncompleteTodos(newIncompleteTodos);
+    const newCompleteTodos = [...completeTodos, incompleteTodos[index]];
+    setCompleteTodos(newCompleteTodos);
+  };
+
+  const onClickBack = (index) => {
+    const newCompleteTodos = [...completeTodos];
+    newCompleteTodos.splice(index, 1);
+    setCompleteTodos(newCompleteTodos);
+    const newIncompleteTodos = [...incompleteTodos, completeTodos[index]];
+    setIncompleteTodos(newIncompleteTodos);
+  };
 
   return (
-    <React.Fragment>
-      <h1 style={{ color: "red" }}>Hello</h1>
-      <ColorfulMessage color="blue" message="Good" />
-      <ColorfulMessage color="pink" message="Bad" />
-      <ColorfulMessage color="red" message="Good" />
-      <button onClick={onClickCountUp}>Up</button>
-      <p>{num}</p>
-      <button onClick={onClickShowFlag}>ON/OFF</button>
-
-      {faceShowFlag && <p>^-^</p>}
-    </React.Fragment>
+    <>
+      <InputTodo
+        todoText={todoText}
+        onChangeTodoText={onChangeTodoText}
+        onClickAdd={onClickAdd}
+      />
+      <IncompleteTodos
+        incompleteTodos={incompleteTodos}
+        onClickComplete={onClickComplete}
+        onClickDelete={onClickDelete}
+      />
+      <CompleteTodos completeTodos={completeTodos} onClickBack={onClickBack} />
+    </>
   );
 };
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,5 @@
 import React from "react";
 import ReactDom from "react-dom";
-import App from "./App";
-
-// const App = () => {
-//   return (
-//     <React.Fragment>
-//       <h1>Hello</h1>
-//       <p>Good</p>
-//     </React.Fragment>
-//   );
-// };
+import { App } from "./App";
 
 ReactDom.render(<App />, document.getElementById("root"));
