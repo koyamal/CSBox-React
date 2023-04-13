3ac7425a0b29554c7b4c4dc72b131f22f0ba476a
koyamal <74910401+koyamal@users.noreply.github.com>
2023-04-13 14:07:28 +0900

    Add styles with Css modules

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { InlineStyle } from "./components/InlineStyle";
+import { CssModules } from "./components/CssModules";
 import "./styles.css";
 
 export const App = () => {
   return (
     <div>
       <InlineStyle />
+      <CssModules />
     </div>
   );
 };
