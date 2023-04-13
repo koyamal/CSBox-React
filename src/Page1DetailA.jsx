41a3ebea1ae0b595c5927c57974f795816d450a4
koyamal <74910401+koyamal@users.noreply.github.com>
2023-04-13 18:58:20 +0900

    Use useHistory for routing

--- src/Page1DetailA.jsx
+++ src/Page1DetailA.jsx
@@ -1,11 +1,18 @@
-import { useLocation } from "react-router-dom";
+import { useLocation, useHistory } from "react-router-dom";
 
 export const Page1DetailA = () => {
   const { state } = useLocation();
+  const history = useHistory();
+
+  const onClickBack = () => {
+    history.goBack();
+  };
+
   console.log(state);
   return (
     <div>
       <h1>Page1DetailA</h1>
+      <button onClick={onClickBack}>Back</button>
     </div>
   );
 };
