019d34dff9f877a70924cd166396b4a5f975a7d6
koyamal <74910401+koyamal@users.noreply.github.com>
2023-04-14 14:18:55 +0900

    Use global state with createContext/useContext

--- src/components/pages/Top.jsx
+++ src/components/pages/Top.jsx
@@ -1,14 +1,19 @@
 import styled from "styled-components";
 import { useHistory } from "react-router-dom";
+import { useContext } from "react";
 import { SecondaryButton } from "../atoms/button/SecondaryButton";
+import { UserContext } from "../../providers/UserProvider";
 
 export const Top = () => {
   const history = useHistory();
+  const { setUserInfo } = useContext(UserContext);
   const onClickAdmin = () => {
-    history.push({ pathname: "/users", state: { isAdmin: true } });
+    setUserInfo({ isAdmin: true });
+    history.push("/users");
   };
   const onClickNormal = () => {
-    history.push({ pathname: "/users", state: { isAdmin: false } });
+    setUserInfo({ isAdmin: false });
+    history.push("/users");
   };
   return (
     <SContainer>
