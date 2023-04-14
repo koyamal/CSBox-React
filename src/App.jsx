cbc77f41c972ecd68ea0da11ed700fb59b5d350f
koyamal <74910401+koyamal@users.noreply.github.com>
2023-04-14 12:15:22 +0900

    Delete user in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,6 @@ import React from "react";
 import { Router } from "./router/Router";
 import "./styles.css";
 
-const user = {
-  image: "https://source.unsplash.com/RUgbGfdma6U",
-  name: "Tom",
-  email: "abcabc@example.com",
-  phone: "123-4567-8910",
-  company: {
-    name: "XXXXX Corporation"
-  },
-  website: "YYYYY.com"
-};
-
 export const App = () => {
   return <Router />;
 };
