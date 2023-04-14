150c9a12314dca1b237fd1b0e1c09398f9d698eb
koyamal <74910401+koyamal@users.noreply.github.com>
2023-04-14 11:31:31 +0900

    Create Organism in Atomic Design

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,19 @@ import React from "react";
 import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
 import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
 import { SearchInput } from "./components/molecules/SearchInput";
+import { UserCard } from "./components/organisms/user/UserCard";
 import "./styles.css";
+
+const user = {
+  image: "https://source.unsplash.com/RUgbGfdma6U",
+  name: "Tom",
+  email: "abcabc@example.com",
+  phone: "123-4567-8910",
+  company: {
+    name: "XXXXX Corporation"
+  },
+  website: "YYYYY.com"
+};
 
 export const App = () => {
   return (
@@ -11,6 +23,7 @@ export const App = () => {
       <SecondaryButton>Test</SecondaryButton>
       <br />
       <SearchInput />
+      <UserCard user={user} />
     </div>
   );
 };
