16c6ee862f2d21090b63e70a6434853db7f99cb4
koyamal <74910401+koyamal@users.noreply.github.com>
2023-04-14 14:31:05 +0900

    Optimize rendering with memo

--- src/components/pages/Users.jsx
+++ src/components/pages/Users.jsx
@@ -1,4 +1,7 @@
+import { useContext } from "react";
 import styled from "styled-components";
+import { UserContext } from "../../providers/UserProvider";
+import { SecondaryButton } from "../atoms/button/SecondaryButton";
 import { SearchInput } from "../molecules/SearchInput";
 import { UserCard } from "../organisms/user/UserCard";
 
@@ -17,10 +20,16 @@ const users = [...Array(10).keys()].map((val) => {
 });
 
 export const Users = () => {
+  const { userInfo, setUserInfo } = useContext(UserContext);
+  const onClickSwitch = () => {
+    setUserInfo({ isAdmin: userInfo ? !userInfo.isAdmin : false });
+  };
   return (
     <SContainer>
       <h2>Users List</h2>
       <SearchInput />
+      <br />
+      <SecondaryButton onClick={onClickSwitch}>Change</SecondaryButton>
       <SUserArea>
         {users.map((user) => {
           return <UserCard key={user.id} user={user} />;
